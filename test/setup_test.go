dd03f43019befb9080d02f8b2d5561a3639d625b
ngwndeLong <lognnh2112@gmail.com>
2025-12-05 12:03:42 +0700

    fix testmain

--- test/setup_test.go
+++ test/setup_test.go
@@ -13,7 +13,6 @@ var TestApp *app.Application
 
 func TestMain(m *testing.M) {
 
-	// ==== ENV for TEST ====
 	os.Setenv("SERVER_PORT", "9999")
 	os.Setenv("DB_HOST", "localhost")
 	os.Setenv("DB_PORT", "5435")
@@ -24,11 +23,27 @@ func TestMain(m *testing.M) {
 	os.Setenv("JWT_SECRET_KEY", "TEST_SECRET_123")
 
 	cfg := config.NewConfig()
-
 	TestApp = app.NewApplication(cfg)
 
 	if TestApp == nil {
 		log.Fatal("Cannot initialize Application")
+	}
+
+	sqlDB := TestApp.DB()
+
+	_, err := sqlDB.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
+	if err != nil {
+		log.Fatal("Failed to enable uuid-ossp:", err)
+	}
+
+	initSQL, err := os.ReadFile("init.sql")
+	if err != nil {
+		log.Fatal("Failed to read init.sql:", err)
+	}
+
+	_, err = sqlDB.Exec(string(initSQL))
+	if err != nil {
+		log.Fatal("Migration failed:", err)
 	}
 
 	code := m.Run()
